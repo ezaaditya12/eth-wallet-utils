d5e76f49580f7232bcb6f533d07907296ce4097e
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-06-25 22:00:36 +0700

    HDWallet generate child wallet

--- src/HDWallet/__tests__/generate.test.js
+++ src/HDWallet/__tests__/generate.test.js
@@ -4,13 +4,14 @@ import { createCases as cases } from './test-cases';
 
 const log = console.log;
 
-describe('Create HD Wallet', () => {
-  it('Should create success with mnemonic phrase', () => {
+describe('Generate Child Wallet', () => {
+  it('Should generate success with mnemonic phrase', () => {
     const hdWallet = HDWallet.fromMnemonic(cases.success.mnemonic);
     const address = new Wallet(hdWallet.privateKey).getAddress();
-    // log('[publicKey]', hdWallet.publicKey);
-    // log('[privateKey]', hdWallet.privateKey);
-    // log('[address]', new Wallet(hdWallet.privateKey).address);
+
+    const childAddressArr = HDWallet.generate({from: 0, total: 10})(hdWallet);
+    log('[childAddressArr]', childAddressArr);
+
     expect(address).toBe('0xE6EA8442BD58A0241a50f088eDEb5a0C99bfA888');
   });
 });
--- src/HDWallet/index.js
+++ src/HDWallet/index.js
@@ -1,20 +1,35 @@
-import { HDNode } from 'ethers';
+import { HDNode, Wallet } from 'ethers';
 import mnGen from 'mngen';
 
 const log = console.log;
 
 class HDWallet {
-  static fromMnemonic(mnemonic){
+
+  static fromMnemonic(mnemonic) {
     return HDNode.fromMnemonic(mnemonic);
   }
 
-  static newOne(){
-    const mnemonic = mnGen.word(12, ' ');
-    log('[HD Wallet][mnemonic]', mnemonic);
-    return HDNode.fromMnemonic(mnemonic);
+  static newMnemonic() {
+    return mnGen.word(12, ' ');
   }
 
-  
+  static newOne() {
+    return HDNode.fromMnemonic(mnGen.word(12, ' '));
+  }
+
+  static generate({ from, total }) {
+    return masterNode =>
+      Array(total)
+        .fill(null)
+        .map((_, index) => {
+          const childPath = `${HDWallet.BIP44}/${from + index}`;
+          const childNode = masterNode.derivePath(childPath);
+          const wallet = new Wallet(childNode.privateKey);
+          return wallet.address;
+        });
+  }
 }
+
+HDWallet.BIP44 = 'm/44\'/60\'/0\'/0';
 
 export default HDWallet;
